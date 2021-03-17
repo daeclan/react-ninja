26a860095620e5ad85d8a39e2e751c9b667cdc2e
Declan Murphy <declxn@gmail.com>
2021-03-17 18:16:25 -0500

    Introduced variables into the App.js and JSX

--- dingo-blog/src/App.js
+++ dingo-blog/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the dingo blog, mate';
+  const likes = 50
+
   return (
     <div className="App">
       <div className="content">
         <h1>
-          App Component
+          {title}
+          <p>Liked {likes} different variations of dingo</p>
         </h1>
       </div>
     </div>
