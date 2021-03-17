948c3ab5d62aded91f28d45295b1d9a0413ffe87
Declan Murphy <declxn@gmail.com>
2021-03-17 18:56:15 -0500

    Added dynamic link using a variable in the href instead of a string.

--- dingo-blog/src/App.js
+++ dingo-blog/src/App.js
@@ -4,6 +4,7 @@ function App() {
   const title = 'Welcome to the dingo blog, mate';
   const likes = 50
   // const dingo = { name: 'dingo', age: '5' }
+  const link = "http://www.dingo.com"
 
   return (
     <div className="App">
@@ -13,6 +14,11 @@ function App() {
           <p>Liked {likes} different variations of dingo</p>
           {/* <p>{dingo}</p> */}
         </h1>
+        <p>{10}</p>
+        <p>{"Big Dingos"}</p>
+        <p>{[2, 0, 7, 2, 4, 2, 5, 6, 4, 3]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Dingo Link</a>
       </div>
     </div>
   );
