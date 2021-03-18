791cf166dabec3485b6f6038a2df17d8a3191733
Declan Murphy <declxn@gmail.com>
2021-03-18 18:18:25 -0500

    Added new component to component tree (navbar) and cleaned up homepage a little.

--- dingo-blog/src/App.js
+++ dingo-blog/src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   const title = 'Welcome to the dingo blog, mate';
   const likes = 50
-  // const dingo = { name: 'dingo', age: '5' }
   const link = "http://www.dingo.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>
           {title}
           <p>Liked {likes} different variations of dingo</p>
-          {/* <p>{dingo}</p> */}
         </h1>
-        <p>{10}</p>
         <p>{"Big Dingos"}</p>
-        <p>{[2, 0, 7, 2, 4, 2, 5, 6, 4, 3]}</p>
         <p>{Math.random() * 10}</p>
         <a href={link}>Dingo Link</a>
       </div>
