b5a0b1de2b9f5c5f80be5cf4e9d44cf4f08c1d44
Declan Murphy <declxn@gmail.com>
2021-03-22 11:46:09 -0500

    First iteration fails, trying to pass Name into the function breaks it. Must be an anonymous function that invokes it.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
     console.log("DINGO")
   }
 
+  const handleClickAgain = (name) => {
+    console.log('hello' + name)
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me, Dingo</button>
+      <button onClick={handleClickAgain('mario')}>Click me a second time, dingo</button>
     </div>
   );
 }
