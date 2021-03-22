8c51e2ec48a5d5f19e0849b1c6dfb93bf9c325eb
Declan Murphy <declxn@gmail.com>
2021-03-22 11:48:29 -0500

    Fixed function by passing in 'mario' below, not above.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -5,14 +5,16 @@ const Home = () => {
   }
 
   const handleClickAgain = (name) => {
-    console.log('hello' + name)
+    console.log('hello ' + name)
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me, Dingo</button>
-      <button onClick={handleClickAgain('mario')}>Click me a second time, dingo</button>
+      <button onClick={() => {
+        handleClickAgain('mario')
+      }}>Click me a second time, dingo</button>
     </div>
   );
 }
