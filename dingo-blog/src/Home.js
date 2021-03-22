aa6babf212875b746def0032496c88972ebd105e
Declan Murphy <declxn@gmail.com>
2021-03-22 11:29:04 -0500

    Added clickable button that reacts with an anonymous function.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log("DINGO")
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me, Dingo</button>
     </div>
   );
 }
