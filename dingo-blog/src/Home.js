9534247576ea765faadd06376247514025231be7
Declan Murphy <declxn@gmail.com>
2021-03-24 16:59:13 -0500

    Added a second useStae method for age, an integer, and updated that as well.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -3,15 +3,17 @@ import { useState } from 'react';
 const Home = () => {
   // let name = 'dingiooo';
   const [name, setName] = useState('dingiooo')
+  const [age, setAge] = useState(25)
 
   const handleClick = () => {
     setName('dingini');
+    setAge('31')
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click Me, Dingo</button>
     </div>
   );
