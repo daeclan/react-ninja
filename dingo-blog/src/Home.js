3d0ee6513236c91f3dc264fc0139e760200e9fb2
Declan Murphy <declxn@gmail.com>
2021-03-24 16:19:06 -0500

    Set up a name change but didn't set up useState hook, so the name only changed in the console.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -1,18 +1,15 @@
 const Home = () => {
+  let name = 'dingiooo';
 
-  const handleClick = (e) => {
-    console.log("DINGO", e)
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target)
+  const handleClick = () => {
+    name = 'dingigi'
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>Click Me, Dingo</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>Click me a second time, dingo</button>
     </div>
   );
 }
