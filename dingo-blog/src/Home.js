0b8c4ced76641785943aad072eb9ceb04209199c
Declan Murphy <declxn@gmail.com>
2021-03-25 21:11:47 -0500

    Reset template for tomorrow's lesson.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -1,20 +1,10 @@
 import { useState } from 'react';
 
 const Home = () => {
-  // let name = 'dingiooo';
-  const [name, setName] = useState('dingiooo')
-  const [age, setAge] = useState(25)
-
-  const handleClick = () => {
-    setName('dingini');
-    setAge('31')
-  }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click Me, Dingo</button>
     </div>
   );
 }
