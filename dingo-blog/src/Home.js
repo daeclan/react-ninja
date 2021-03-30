e91c26cdac0a6e6735ba4bea393c6520f108bdde
Declan Murphy <declxn@gmail.com>
2021-03-29 21:30:58 -0500

    Used useState to store a list and JSX to map through the array of objects, utilizing the object's key to track the extent of the map.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-
+  const [blogs, setBlogs] = useState([
+    { title: 'Dingo 1', body: 'lorem ipsum', author: 'dingiooo', id: 1 },
+    { title: 'Dingo 2', body: 'lorem ipsum', author: 'doskus', id: 2 },
+    { title: 'Dingo 3', body: 'lorem ipsum', author: 'dungo', id: 3 }
+  ])
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
