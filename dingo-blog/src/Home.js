8f78b82e8391af1a86887bdf8573accf303f67ed
Declan Murphy <declxn@gmail.com>
2021-04-06 09:44:02 -0500

    Added name as a dependency we added to the second argument.

--- dingo-blog/src/Home.js
+++ dingo-blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Dingo 3', body: 'lorem ipsum', author: 'dingiooo', id: 3 }
   ])
 
+  const [name, setName] = useState('dingiooo')
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,11 +17,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log('ive run')
-  }, [])
+    console.log(name)
+  }, [name])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blog Posts" handleDelete={handleDelete} />
+      <button onClick={() => setName('doskus')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
