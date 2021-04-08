a8bd9f2a92254104a3b1a7ffe5f93e473e3b5966
Declan Murphy <declxn@gmail.com>
2021-04-07 19:03:49 -0500

    Removed leftover code and other artifacts.

--- dingo-blog/src/App.js
+++ dingo-blog/src/App.js
@@ -2,17 +2,12 @@ import Navbar from './Navbar'
 import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the dingo blog, mate';
-  const likes = 50
-  const link = "http://www.dingo.com"
 
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Dingo Link</a>
       </div>
     </div>
   );
